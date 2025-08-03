ef2895778448268b23a48b89b7ef55a50defa678
Piotr Chmielowiec <mbpro@MacBook-Pro-Piotr.local>
2025-08-03 20:32:10 +0200

    implemented cpu-load-app infrastructure

---
+++ cpu-load-app/test.js
@@ -0,0 +1,30 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export let options = {
+  stages: [{ duration: "15m", target: 500 }],
+  thresholds: {
+    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(95)<800"],
+    checks: ["rate>0.99"],
+  },
+};
+
+const BASE_URL = "http://localhost:4000";
+
+export default function () {
+  const params = {
+    timeout: "200ms",
+  };
+
+  const res = http.get(`${BASE_URL}/uuid`, params);
+
+  check(res, {
+    "✅ status is 200": (r) => r.status === 200,
+    "✅ response body is not empty": (r) => r.body && r.body.length > 0,
+    "✅ content-type header is correct": (r) =>
+      r.headers["Content-Type"] === "application/json",
+  });
+
+  sleep(0.1);
+}
